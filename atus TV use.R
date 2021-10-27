c35e04ec34af8fd5afda79c1fd59810255b5f8ee
coudykid <coudykid@gmail.com>
2021-10-27 16:13:38 -0400

    Delete atus TV use.R

--- atus TV use.R
+++
@@ -1,6 +0,0 @@
-
-# trcode (activity file)
-    # 12 Socializing, Relaxing, and Leisure
-        # 03 Reading and Leisure
-          # 03 Television and movies (not religious)
-          # 04 Television (religious)
